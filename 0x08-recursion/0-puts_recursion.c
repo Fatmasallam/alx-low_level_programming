9a8b250065bb07263e02a6c53b9ba965dac5e751
Fatmasallam <fatmasallam2@hotmail.com>
2024-01-11 07:27:33 -0800

    newww

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - fun
+ * @s:parameter
+ * Return:0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+
+	{
+
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
+
+	}
+	else
+	{
+	_putchar('\n');
+	}
+
+}
