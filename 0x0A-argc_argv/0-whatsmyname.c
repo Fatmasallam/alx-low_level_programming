0ce69caf628f23fbc631086a4494f9658d3ab20e
Fatmasallam <fatmasallam2@hotmail.com>
2024-01-14 05:15:48 -0800

    new com

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+		
+#include "main.h"
+		
+
+		
+/**
+		
+ * main -to print
+ *fatmasallam
+ * @argc: num
+ * @argv: array		
+ * Return: 0		
+ */
+		
+int main(int argc, char *argv[])
+		
+{
+	(void)argc;
+
+	printf("%s\n", *argv[0]);
+		
+	return (0);
+		
+}
